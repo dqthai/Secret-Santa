6d65211238a8d5d0eada2287cd594d7dc503f598
Dan Thai <d_thai94@yahoo.com>
2014-11-04 10:26:51 -0800

    text test

--- process.php
+++ process.php
@@ -2,8 +2,9 @@
 $name = $_POST['name'];
 $number = $_POST['number'];
 	foreach(array_keys($_POST['name']) as $key) {
-		echo "$name[$key]";
-		echo "$number[$key]";
+		echo "$name[$key] \n";
+		echo "$number[$key] \n";
 	}
 	echo "hi there";
+	mail( '4089812708@vtext.com', '', 'Testing');
 ?>
