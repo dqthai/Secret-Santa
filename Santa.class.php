bf6de59a7e7b95b73efeda552f072cbb95bd1a23
Dan Thai <d_thai94@yahoo.com>
2014-12-10 00:48:48 -0800

    debugging

--- Santa.class.php
+++ Santa.class.php
@@ -145,34 +145,30 @@ Class SecretSanta {
 	private function sendEmails($assigned_users){
 		//For each user
 		foreach($assigned_users as $giver){
-			//Send the following email
-      echo "hi <br>";
-      $mail->isSMTP();                                      // Set mailer to use SMTP
-      $mail->Host = 'smtp.mailgun.org';                     // Specify main and backup SMTP servers
-      $mail->SMTPAuth = true;                               // Enable SMTP authentication
-      $mail->Username = 'postmaster@app31198679.mailgun.org';   // SMTP username
-      $mail->Password = 'b3144f3ea73261ff891b103fc31108e0';                           // SMTP password
-      $mail->SMTPSecure = 'tls';                            // Enable encryption, only 'tls' is accepted
+        $mail = new PHPMailer;
 
-      $mail->From = 'me@app31198679.mailgun.org';
-      $mail->FromName = 'Mailer';
-      $mail->addAddress($giver['email']);                 // Add a recipient
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.mailgun.org';                     // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'postmaster@app31198679.mailgun.org';   // SMTP username
+        $mail->Password = 'b3144f3ea73261ff891b103fc31108e0';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable encryption, only 'tls' is accepted
 
-      $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+        $mail->From = 'me@app31198679.mailgun.org';
+        $mail->FromName = 'Mailer';
+        $mail->addAddress($giver['email']);                 // Add a recipient
 
-      $mail->Subject = 'Secret Santa';
-   /*   $mail->Body    = "Hello {$giver['name']}, 
-				For Secret Santa this year you will be buying a present for {$giver['giving_to']['name']} 
-				Presents should all be around ${$this->item_value},
-				Good luck and Merry Christmas, Santa";
-     */
-     $mail->Body = 'Testing some Mailgun awesomness';
-      if(!$mail->send()) {
-          echo 'Message could not be sent.<br>';
-          echo 'Mailer Error: ' . $mail->ErrorInfo;
-      } else {
-          echo 'Message has been sent<br>';
-      }
+        $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+
+        $mail->Subject = 'Hello';
+        $mail->Body    = 'Testing some Mailgun awesomness';
+
+        if(!$mail->send()) {
+            echo 'Message could not be sent.';
+            echo 'Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+            echo 'Message has been sent';
+        }
     }	
  }
 	/**
